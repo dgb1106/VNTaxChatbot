7ec748c2076deb8d1f001b98d34be1bf6d8db7be
dgb1106 <duonggiabao1106@gmail.com>
2024-11-04 21:16:05 +0700

    Add instruction about context

--- EnvironmentVariables.py
+++ EnvironmentVariables.py
@@ -4,5 +4,7 @@ import os
 load_dotenv()
 
 gemini_api_key = os.getenv("API_KEY")
+default_language = os.getenv("DEFAULT_LANGUAGE")
 
 print("API_KEY:", gemini_api_key)
+print("DEFAULT_LANGUAGE:", default_language)
--- TestAPI.py
+++ TestAPI.py
@@ -3,9 +3,22 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+api_key = os.getenv("API_KEY")
+instruction = os.getenv("INSTRUCTION")
 
-genai.configure(api_key=os.getenv("API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash")
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel(
+    model_name="gemini-1.5-flash",
+    system_instruction=instruction
+)
 
-response = model.generate_content("Write a story about a magic backpack.")
-print(response.text)
+def call_llm_api(question):
+    response = model.generate_content(question)
+    print(response.text)
+
+def main():
+    s = input("Please enter your question:")
+    call_llm_api(s)
+    
+if __name__ == "__main__":
+    main()
